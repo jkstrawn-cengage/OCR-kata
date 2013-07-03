528b22f95f199155fe93d33f9797c437d7d120d6
Jeremy Strawn <jkstrawn@gmail.com>
2013-07-03 10:29:20 -0400

    translates all numbers - refactored

--- src/OCRBank.java
+++ src/OCRBank.java
@@ -1,24 +1,79 @@
 
 public class OCRBank {
+	static String numberPatterns[][] = new String[10][3];
+	static int numbersToCheck = 10;
+	static int linesPerNumber = 3;
+	
+	public static void initiatePatterns() {
+		numberPatterns[0][0] = " _ ";
+		numberPatterns[0][1] = "| |";
+		numberPatterns[0][2] = "|_|";
+		
+		numberPatterns[1][0] = "   ";
+		numberPatterns[1][1] = "  |";
+		numberPatterns[1][2] = "  |";
+		
+		numberPatterns[2][0] = " _ ";
+		numberPatterns[2][1] = " _|";
+		numberPatterns[2][2] = "|_ ";
+		
+		numberPatterns[3][0] = " _ ";
+		numberPatterns[3][1] = " _|";
+		numberPatterns[3][2] = " _|";
+		
+		numberPatterns[4][0] = "   ";
+		numberPatterns[4][1] = "|_|";
+		numberPatterns[4][2] = "  |";
+		
+		numberPatterns[5][0] = " _ ";
+		numberPatterns[5][1] = "|_ ";
+		numberPatterns[5][2] = " _|";
+		
+		numberPatterns[6][0] = " _ ";
+		numberPatterns[6][1] = "|_ ";
+		numberPatterns[6][2] = "|_|";
+	
+		numberPatterns[7][0] = " _ ";
+		numberPatterns[7][1] = "  |";
+		numberPatterns[7][2] = "  |";
+		
+		numberPatterns[8][0] = " _ ";
+		numberPatterns[8][1] = "|_|";
+		numberPatterns[8][2] = "|_|";
+		
+		numberPatterns[9][0] = " _ ";
+		numberPatterns[9][1] = "|_|";
+		numberPatterns[9][2] = " _|";	
+	}
 
 	public static Object translate(String input) {
+		initiatePatterns();
 		String lines[] = input.split("\n");
 		String computedNumbers = "";
-		int numOfNumbers = (int) Math.floor(lines[0].length() / 3.0);
-		for (int i = 0; i < numOfNumbers; i++) {
-			String letterExpected1 = " _ ";
-			String letterActual1 = lines[0].substring(i * 3, i * 3 + 3);
-			String letterExpected2 = "| |";
-			String letterActual2 = lines[1].substring(i * 3, i * 3 + 3);
-			String letterExpected3 = "|_|";
-			String letterActual3 = lines[2].substring(i * 3, i * 3 + 3);
-			
-			if (letterActual1.equals(letterExpected1) && letterActual2.equals(letterExpected2) && letterActual3.equals(letterExpected3)) {
-				computedNumbers += "0";
+		int numOfNumbers = getNumberOfNumbersInFile(lines);
+		for (int indexInFile = 0; indexInFile < numOfNumbers; indexInFile++) {
+			for (int numberToCompare = 0; numberToCompare < numbersToCheck; numberToCompare++) {
+				String[] letterActuals = new String[3];
+				String[] letterExpecteds = new String[3];
+				for (int lineNumber = 0; lineNumber < linesPerNumber; lineNumber++) {
+					letterExpecteds[lineNumber] = numberPatterns[numberToCompare][lineNumber];
+					letterActuals[lineNumber] = getNumberSegmentFromLine(lines[lineNumber], indexInFile);
+				}
+				if (letterActuals[0].equals(letterExpecteds[0]) && letterActuals[1].equals(letterExpecteds[1]) && 
+						letterActuals[2].equals(letterExpecteds[2])) {
+					computedNumbers += Integer.toString(numberToCompare);
+				}
 			}
-			
 		}
 		return computedNumbers;
 	}
 
+	public static int getNumberOfNumbersInFile(String[] lines) {
+		double numberOfNumberWithOverflow = lines[0].length() / 3.0;
+		return (int) Math.floor(numberOfNumberWithOverflow);
+	}
+	
+	public static String getNumberSegmentFromLine(String line, int indexInFile) {
+		return line.substring(indexInFile * 3, indexInFile * 3 + 3);
+	}
 }
--- src/OCRBankTest.java
+++ src/OCRBankTest.java
@@ -6,10 +6,119 @@ public class OCRBankTest {
 
 	@Test
 	public void test() {
-		String input = " _  _  _  _  _  _  _  _  _ \n" + 
-					   "| || || || || || || || || |\n" + 
-					   "|_||_||_||_||_||_||_||_||_|\n";
+		String input = 
+					" _  _  _  _  _  _  _  _  _ \n" + 
+					"| || || || || || || || || |\n" + 
+					"|_||_||_||_||_||_||_||_||_|\n";
 		assertEquals("000000000", OCRBank.translate(input));
 	}
+	
+	@Test
+	public void testone() throws Exception {
+		String input = 
+					"                           \n" +
+					"  |  |  |  |  |  |  |  |  |\n" +
+					"  |  |  |  |  |  |  |  |  |\n";
+		assertEquals("111111111", OCRBank.translate(input));
+	}
 
+	@Test
+	public void testtwo() throws Exception {
+		String input =
+					" _  _  _  _  _  _  _  _  _ \n" +
+					" _| _| _| _| _| _| _| _| _|\n" +
+					"|_ |_ |_ |_ |_ |_ |_ |_ |_ \n";
+		assertEquals("222222222", OCRBank.translate(input));
+	}
+	
+	@Test
+	public void test3() throws Exception {
+		String input = 
+					" _  _  _  _  _  _  _  _  _ \n" +
+					" _| _| _| _| _| _| _| _| _|\n" +
+					" _| _| _| _| _| _| _| _| _|\n";
+		assertEquals("333333333", OCRBank.translate(input));
+	}
+	
+	@Test
+	public void test4() throws Exception {
+		String input = 
+					"                           \n" +
+					"|_||_||_||_||_||_||_||_||_|\n" +
+					"  |  |  |  |  |  |  |  |  |\n";
+		assertEquals("444444444", OCRBank.translate(input));
+	}
+	
+	@Test
+	public void test5() throws Exception {
+		String input = 
+						" _  _  _  _  _  _  _  _  _ \n" +
+						"|_ |_ |_ |_ |_ |_ |_ |_ |_ \n" +
+						" _| _| _| _| _| _| _| _| _|\n";
+		assertEquals("555555555", OCRBank.translate(input));
+	}
+	
+	@Test
+	public void test6() throws Exception {
+		String input = 
+						" _  _  _  _  _  _  _  _  _ \n" +
+						"|_ |_ |_ |_ |_ |_ |_ |_ |_ \n" +
+						"|_||_||_||_||_||_||_||_||_|\n";
+		assertEquals("666666666", OCRBank.translate(input));
+	}
+	
+	@Test
+	public void test7() throws Exception {
+		String input = 
+						" _  _  _  _  _  _  _  _  _ \n" +
+						"  |  |  |  |  |  |  |  |  |\n" +
+						"  |  |  |  |  |  |  |  |  |\n";
+		assertEquals("777777777", OCRBank.translate(input));
+	}
+	
+	@Test
+	public void test8() throws Exception {
+		String input = 
+						" _  _  _  _  _  _  _  _  _ \n" +
+						"|_||_||_||_||_||_||_||_||_|\n" +
+						"|_||_||_||_||_||_||_||_||_|\n";
+		assertEquals("888888888", OCRBank.translate(input));
+	}
+	
+	@Test
+	public void test9() throws Exception {
+		String input = 
+						" _  _  _  _  _  _  _  _  _ \n" +
+						"|_||_||_||_||_||_||_||_||_|\n" +
+						" _| _| _| _| _| _| _| _| _|\n";
+		assertEquals("999999999", OCRBank.translate(input));
+	}
+	
+	@Test
+	public void testAllNumbers() throws Exception {
+		String input = 
+						"    _  _     _  _  _  _  _ \n" +
+						"  | _| _||_||_ |_   ||_||_|\n" +
+						"  ||_  _|  | _||_|  ||_| _|\n";
+		assertEquals("123456789", OCRBank.translate(input));
+	}
+	
+	@Test
+	public void testOsAndAFive() throws Exception {
+		String input = 
+						" _  _  _  _  _  _  _  _    \n" +
+						"| || || || || || || ||_   |\n" + 
+						"|_||_||_||_||_||_||_| _|  |\n";
+		assertEquals("000000051", OCRBank.translate(input));
+	}
+	/*
+	@Test
+	public void testMissingPipeInFive() throws Exception {
+		String input = 
+						"    _  _  _  _  _  _     _ \n" +
+						"|_||_|| || ||_   |  |  | _ \n" +
+						"  | _||_||_||_|  |  |  | _|\n";
+		assertEquals("49006771? ILL", OCRBank.translate(input));
+	}
+	*/
 }
